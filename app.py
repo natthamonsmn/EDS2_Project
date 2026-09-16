45979783917487757700925a9282a02f107c411e
natthamonsmn <beamnattipad12@gmail.com>
2026-09-16 07:19:00 +0000

    Add DataFrame

---
+++ app.py
@@ -0,0 +1,6 @@
+import pandas
+print('Hello World')
+print('This is a sample Python application.')
+df = pandas.DataFrame({'A' : [1, 2, 3], 'B' : [4, 5, 6]})
+print('My Firts DataFrame :')
+print(df)
